b7fa604e3ddb2fc5ecc15c595eb6840b18011731
Shipa <shipa-dev@shipa.io>
2022-04-27 04:15:42 +0000

    Release 1.7.5

--- Formula/shipa-cli.rb
+++ Formula/shipa-cli.rb
@@ -2,15 +2,15 @@ class ShipaCli < Formula
     desc "Shipa command-line interface"
     homepage "https://shipa.io/"
     license "Apache-2.0"
-    version "1.7.4"
+    version "1.7.5"
 
     platform = ""
     sha = ""
     root_url = "https://storage.googleapis.com/shipa-client/" + version + "/shipa_"
 
-    darwin_amd64_sha256 = "5d5d872cfe44e8878ca785e3ffb293bbe931a4cfaba0dd78b6ea567ed740ac51"
-    linux_amd64_sha256 = "2a6eefeeeb2d80ae4f060f4c17a51a9b44494f18b206a40bef806bae38c459dd"
-    linux_386_sha256 = "f4cf10305a719dcbdf9332e59f40d61ebdacbf55cc5862830240d3e1704277a7"
+    darwin_amd64_sha256 = "33bc041392436e503fb35627c367650b0615812b34f615b6d0c323337dc52190"
+    linux_amd64_sha256 = "91e46edad1e0205fc0a105c8389129af8cec93b2f09988e6e6984b3eabf2f83b"
+    linux_386_sha256 = "4282ee4b544da8a805fea1cc03c57e39cdeea42bb02970dbb456238988ad8088"
 
     os = OS.kernel_name.downcase
     arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
