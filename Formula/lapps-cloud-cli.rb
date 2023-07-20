16c89367011f24f9d42ce5ac7c5b74e4d9993d12
Shipa <shipa-dev@shipa.io>
2023-07-20 16:51:19 +0000

    Release 1.8.2-cloud

--- Formula/lapps-cloud-cli.rb
+++ Formula/lapps-cloud-cli.rb
@@ -2,15 +2,15 @@ class LappsCloudCli < Formula
     desc "Lens Apps cloud command-line interface"
     homepage "https://apps.lenscloud.io//"
     license "Apache-2.0"
-    version "1.8.1-cloud"
+    version "1.8.2-cloud"
 
     platform = ""
     sha = ""
     root_url = "https://storage.googleapis.com/shipa-client-cloud/" + version + "/lapps_"
 
-    darwin_amd64_sha256 = "86eefb7270996618ac163da1098e47a50f16763125a11521ab77e5f96ef8bc56"
-    linux_amd64_sha256 = "abba62ccf535f024e6876c324c4bea09f883946ee49644b512cbc4ca0d99024f"
-    linux_386_sha256 = "9424a1b91c5c6abe1d8f511f5112c2833668f25bdca30f0ff2d807d75becabfc"
+    darwin_amd64_sha256 = "c49cbd08c149b2ea327b9889d39add6693db23b258929dd8a518a8a42e4310d4"
+    linux_amd64_sha256 = "6f40e83fb34bc8ddb8ce321d29c8be7dd922d551b604fbb83b648f318cd65785"
+    linux_386_sha256 = "bb5d59c09c69c2ff48ce6705d5ec72c002c96fac0669b22dd4abbd60dd12c553"
 
     os = OS.kernel_name.downcase
     arch = Hardware::CPU.intel? ? "amd64" : Hardware::CPU.arch.to_s
